51751e679e840b1372a78fe4e79765d1ced6fa37
TANABE Hiroyasu <h@cosat.com>
2020-12-09 17:19:18 +0900

    B2C ユーザーフローへの遷移をボタンに変更

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,11 +30,6 @@ namespace individualb2c.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
